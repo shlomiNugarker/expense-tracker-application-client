18b35c2d146ba070e696b05bcf1145673bf7dc44
Shlomi Nugarker <shlomin1231@gmail.com>
2024-06-07 13:05:34 +0300

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+import { SignupPage } from './pages/SignupPage'
+
 function App() {
-  return <>hey</>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+            <li>
+              <Link to="/signup">signup</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
