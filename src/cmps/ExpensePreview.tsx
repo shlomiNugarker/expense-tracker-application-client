1f81bd8e71a4a57284594fb76a3e66f1510528ca
Shlomi Nugarker <shlomin1231@gmail.com>
2024-06-08 00:07:26 +0300

    ExpensePreview

---
+++ src/cmps/ExpensePreview.tsx
@@ -0,0 +1,29 @@
+import { useOutletContext } from 'react-router-dom'
+import { Expense } from '../interfaces/Expense'
+import { ContextProps } from '../pages/HomePage'
+
+interface Props {
+  expense: Expense
+}
+
+export const ExpensePreview = ({ expense }: Props) => {
+  const { onDeleteExpense } = useOutletContext<ContextProps>()
+
+  const formattedDate = new Date(expense.date).toLocaleDateString()
+
+  return (
+    <div className="expense-preview">
+      <div className="expense-preview__details">
+        <h2>{expense.title}</h2>
+        <p className="expense-preview__amount">${expense.amount.toFixed(2)}</p>
+        <p className="expense-preview__date">{formattedDate}</p>
+      </div>
+      <button
+        className="expense-preview__delete"
+        onClick={() => expense._id && onDeleteExpense(expense._id)}
+      >
+        Delete
+      </button>
+    </div>
+  )
+}
